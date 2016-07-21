3b858351ee4a662d5b50a19ecca4163f122659c5
Curtis Hughey <hugheyc@uchicago.edu>
2016-07-21 15:45:28 -0400

    Rearranged KGS stuff.  Still would need to manually download some jars if doing a new installation

--- install.sh
+++ install.sh
@@ -3,9 +3,12 @@
 # Adds necessary directories and compiles default version
 # UNTESTED.  Will also want to add kgs stuff ^^^
 
+kgsGtpDir="kgsGtp"
+
 mkdir configs
 mkdir output
 mkdir log
+mkdir "$kgsGtpDir/log" 
 
 chmod u+x build.sh  # Might not need
 ./build.sh  # Default
