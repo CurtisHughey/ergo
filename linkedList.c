954e66d1663ffe066a79f5d413b051bc52f08750
Curtis Hughey <hugheyc@uchicago.edu>
2016-07-25 12:55:04 -0400

    Added the rest of the list functions, now I get to unit test them

--- linkedList.c
+++ linkedList.c
@@ -22,7 +22,6 @@ int add(Node **head, HASHVALUETYPE hashValue) {
 	} 
 
 	// Now just inserts the hashValue at the head position
-
 	Node *newNode = malloc(sizeof(Node));
 	if (!newNode) {
 		ERROR_PRINT("Failed to allocate");
@@ -37,13 +36,73 @@ int add(Node **head, HASHVALUETYPE hashValue) {
 }
 
 // Returns 1 if exists, 0 if not
-int search(Node **head, HASHVALUETYPE hashValue);
+int contains(Node **head, HASHVALUETYPE hashValue) {
+	Node *current = *head;
+	while (current) {
+		if (current->hashValue == hashValue) {
+			return 1;
+		} else {  // Otherwise, keep going
+			current = current->next;
+		}
+	}
+
+	// If we made it to here, no matches
+	return 0;
+}
 
 // Returns the length of the list (0 if none)
-int length(Node **head);
+int length(Node **head) {
+	int length = 0;
 
-// Returns 0 if not already present, other if already present (and will give error message)
-int delete(Node **head, HASHVALUETYPE hashValue);
+	Node *current = *head;
+	while (current) {
+		current = current->next;
+		length++;
+	}	
+
+	return length;
+}
+
+// Returns 0 if successfully deletes, other if not (and will give error message)
+int delete(Node **head, HASHVALUETYPE hashValue) {
+	if (!head) {  // This would be pretty bad
+		ERROR_PRINT("Failed to delete node");
+		return 1;
+	}
+
+	if (!(*head)) {  // Then empty
+		ERROR_PRINT("Trying to delete from empty list");
+		return 1;
+	}
+
+	// Now check if at start of list
+	if ((*head)->hashValue == hashValue) {
+		Node *temp = (*head)->next;
+		free(*head);
+		*head = temp;
+		return 0;
+	}
+
+	// Now just a regular traversal, but we also need to remember the previous
+	Node *prev = *head;
+	Node *current = prev->next;
+
+	while (current) {
+		if (current->hashValue == hashValue) {
+			Node *temp = current->next;
+			free(current);
+			current = NULL;
+			prev->next = temp;
+			return 0;
+		} else {  // Otherwise, keep going
+			prev = current;
+			current = current->next;
+		}
+	}
+
+	// If we made it to here, no matches, we failed
+	return 1;
+}
 
 // Returns 0 if successful, other if not.  Deletes all entries.
 int flush(Node **head) {
--- linkedList.h
+++ linkedList.h
@@ -18,12 +18,12 @@ typedef struct Node {
 int add(Node **head, HASHVALUETYPE hashValue);
 
 // Returns 1 if exists, 0 if not
-int search(Node **head, HASHVALUETYPE hashValue);
+int contains(Node **head, HASHVALUETYPE hashValue);
 
 // Returns the length of the list (0 if none)
 int length(Node **head);
 
-// Returns 0 if not already present, other if already present (and will give error message)
+// Returns 0 if successfully deletes, other if not (and will give error message)
 int delete(Node **head, HASHVALUETYPE hashValue);
 
 // Returns 0 if successful, other if not.  Deletes all entries.
